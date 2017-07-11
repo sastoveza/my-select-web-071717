549fea2722370d821a61d087d170cef75ca9e2e5
Stacey Astoveza <staceyastoveza@transfix.io>
2017-07-11 20:37:06 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ new_collection = []
+  while i < collection.length
+    if yield(collection[i])
+      new_collection << collection [i]
+    end
+    i += 1
+    end
+    new_collection
 end
